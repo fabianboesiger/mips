5538a979d5cc9c7ccb3fc39591ecb2c624dc63c9
Fabian Bösiger <fabian@ThinkPad-T470.home>
2019-05-03 10:43:24 +0200

    Added sadrgb.asm

--- sum.asm
+++ sum.asm
@@ -1,10 +1,10 @@
 #	Hardcoded Inputs
-	addi	$a0, $zero, 5		# a0 = a;
-	addi	$a1, $zero, 4		# a1 = b;
+	addi	$a0, $zero, 3		# a0 = a;
+	addi	$a1, $zero, 1		# a1 = b;
 
 #	Initialization
 	addi	$v0, $zero, 0		# v0 = 0
-	slt	$t0, $a1, $a0		# t0 = (a1 < a0) ? 1 : 0
+	slt	$t0, $a1, $a0		# t0 = (b < a) ? 1 : 0
 	addi	$t1, $zero, 1		# t1 = 1
 	beq	$t0, $t1, return	# if (t1 == 1) return
 	addi	$t2, $a0, 0		# t2 = a0
